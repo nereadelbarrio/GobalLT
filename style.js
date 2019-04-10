e05a3c539bde73a8a6aaf5fc2e4447ea6d713ebb
nereadelbarrio <49400164+nereadelbarrio@users.noreply.github.com>
2019-04-10 13:59:30 +0200

    Update style.js

--- style.js
+++ style.js
@@ -23,6 +23,7 @@
 				{ name: 'Título part6', element: 'h4', attributes: { 'class': 'bck-title7'} },
 				{ name: 'Título rojo', element: 'h5', attributes: { 'class': 'bck-title8'} },
 				{ name: 'Título verde', element: 'h5', attributes: { 'class': 'bck-title9'} },
+				{ name: 'Título morado', element: 'h6', attributes: { 'class': 'bck-title10'} },
 				{ name: 'Título video', element: 'h4', attributes: { 'class': 'titlevideo'} },
 				{ name: 'Subtítulo', element: 'span', attributes: { 'class': 'bck-enfasis-1'} },
 				{ name: 'Azul', element: 'span', attributes: { 'class': 'bck-enfasis-2'} },
